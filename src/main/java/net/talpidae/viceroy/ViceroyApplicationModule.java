e8f833110e811e3a75cbc88ef52e8b7de5bd9bda
Jonas Zeiger <jonas.zeiger@talpidae.net>
2017-04-12 21:42:11 +0200

    Adapt to changes in project "base"

--- src/main/java/net/talpidae/viceroy/ViceroyApplicationModule.java
+++ src/main/java/net/talpidae/viceroy/ViceroyApplicationModule.java
@@ -21,16 +21,6 @@ package net.talpidae.viceroy;
 import com.google.inject.AbstractModule;
 import lombok.extern.slf4j.Slf4j;
 import net.talpidae.base.Base;
-import net.talpidae.base.insect.AsyncQueen;
-import net.talpidae.base.insect.Queen;
-import net.talpidae.base.insect.Slave;
-import net.talpidae.base.insect.SyncSlave;
-import net.talpidae.base.insect.config.DefaultQueenSettings;
-import net.talpidae.base.insect.config.DefaultSlaveSettings;
-import net.talpidae.base.insect.config.QueenSettings;
-import net.talpidae.base.insect.config.SlaveSettings;
-import net.talpidae.base.server.DefaultServerConfig;
-import net.talpidae.base.server.ServerConfig;
 import net.talpidae.base.util.Application;
 
 
@@ -47,13 +37,5 @@ public class ViceroyApplicationModule extends AbstractModule
     protected void configure()
     {
         bind(Application.class).to(ViceroyApplication.class);
-
-        bind(SlaveSettings.class).to(DefaultSlaveSettings.class);
-        bind(Slave.class).to(SyncSlave.class);
-
-        bind(QueenSettings.class).to(DefaultQueenSettings.class);
-        bind(Queen.class).to(AsyncQueen.class);  // we don't actually use it
-
-        bind(ServerConfig.class).to(DefaultServerConfig.class);
     }
 }
